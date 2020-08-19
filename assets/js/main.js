8552562904c6d5c1f3934b949efe2fc7c7b86dbe
odify <odify495@gmail.com>
2020-08-20 00:04:22 +0200

    2021-is-still-under-const...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-//Just another JS code 4 simulating moonphases...
-//(STILL UNDER CONSTRUCTION...)
+//Just another JS code 4 simulating moonphases and dates
+
 
 
 function load_moon_phases(obj,callback){
@@ -64,5 +64,98 @@ var configMoon = {
 }
 load_moon_phases(configMoon,example_1)
 
+//DATE 1
+var countDownDate = new Date("Sep 2, 2020 07:22:04").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("nm1").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("nm1").innerHTML = "EXPIRED";
+  }
+}, 1000 );
+//DATE 2
+var countDownDate1 = new Date("Oct 1, 2020 23:05:15").getTime();
+var x = setInterval(function() {
+  var now1 = new Date().getTime();
+  var distance1 = countDownDate1 - now1;
+  var days = Math.floor(distance1 / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance1 % (1000 * 60)) / 1000);
+  document.getElementById("nm2").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance1 < 0) {
+    clearInterval(x);
+    document.getElementById("nm2").innerHTML = "EXPIRED";
+  }
+}, 1000 );
+//DATE 3
+var countDownDate2 = new Date("Oct 31, 2020 15:49:15").getTime();
+var x = setInterval(function() {
+  var now2 = new Date().getTime();
+  var distance2 = countDownDate2 - now2;
+  var days = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance2 % (1000 * 60)) / 1000);
+  document.getElementById("nm3").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance2 < 0) {
+    clearInterval(x);
+    document.getElementById("nm3").innerHTML = "EXPIRED";
+  }
+}, 1000 );
+//DATE 4
+var countDownDate3 = new Date("Nov 30, 2020 10:29:41").getTime();
+var x = setInterval(function() {
+  var now3 = new Date().getTime();
+  var distance3 = countDownDate3 - now3;
+  var days = Math.floor(distance3 / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance3 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance3 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance3 % (1000 * 60)) / 1000);
+  document.getElementById("nm4").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance3 < 0) {
+    clearInterval(x);
+    document.getElementById("nm4").innerHTML = "EXPIRED";
+  }
+}, 1000 );
+//DATE 4
+var countDownDate3 = new Date("Dec 30, 2020 04:28:13").getTime();
+var x = setInterval(function() {
+  var now4 = new Date().getTime();
+  var distance4 = countDownDate4 - now4;
+  var days = Math.floor(distance4 / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance4 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance4 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance4 % (1000 * 60)) / 1000);
+  document.getElementById("nm5").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance4 < 0) {
+    clearInterval(x);
+    document.getElementById("nm5").innerHTML = "EXPIRED";
+  }
+}, 1000 );
 
+// 2021
 
+// Donnerstag, 28. Januar 2021, 20:16:18 Uhr
+// Samstag, 27. Februar 2021, 09:17:24 Uhr
+// Sonntag, 28. März 2021, 20:48:12 Uhr
+// Dienstag, 27. April 2021, 05:31:36 Uhr
+// Mittwoch, 26. Mai 2021, 13:13:54 Uhr
+// Donnerstag, 24. Juni 2021, 20:39:42 Uhr
+// Samstag, 24. Juli 2021, 04:36:54 Uhr
+// Sonntag, 22. August 2021, 14:02:00 Uhr
+// Dienstag, 21. September 2021, 01:54:42 Uhr
+// Mittwoch, 20. Oktober 2021, 16:56:42 Uhr
+// Freitag, 19. November 2021, 09:57:30 Uhr
+// Sonntag, 19. Dezember 2021, 05:35:36 Uhr
